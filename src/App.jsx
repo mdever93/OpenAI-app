53d7bf1ab80673b699c806e24b7f72021aaf5fbb
Michael Dever <mdever@live.ca>
2022-05-18 06:56:32 -0400

    app now gets api key from database

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,28 @@ import ResponseContainer from './components/ResponseContainer';
 import { useState, useEffect } from 'react';
 import { Configuration, OpenAIApi } from "openai";
 import Navbar from './components/Navbar';
-
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, child, get } from "firebase/database";
 
 import './App.scss';
+const firebaseConfig = {
+  apiKey: "AIzaSyApJ5xuBf84rip-ExcgUhYzZR7KrglvWI4",
+  authDomain: "api-database-3d3e6.firebaseapp.com",
+  databaseURL: "https://api-database-3d3e6-default-rtdb.firebaseio.com",
+  projectId: "api-database-3d3e6",
+  storageBucket: "api-database-3d3e6.appspot.com",
+  messagingSenderId: "946428004924",
+  appId: "1:946428004924:web:f00918afd72b65f9c35139",
+  measurementId: "G-02QM3KSRZ7"
+};
+// TODO: Replace with your app's Firebase project configuration
+
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+const database = getDatabase(app);
+
+
 
 const useLocalStorage = (storageKey, fallbackState) => {
   const [value, setValue] = useState(
@@ -25,12 +44,12 @@ function App() {
   const [currentPrompt, setCurrentPrompt] = useState(null)
   const [currentResponse, setCurrentResponse] = useState({})
   const [allResponses, setAllResponses] = useLocalStorage('allResponses', [])
-  const currentResponseObject = {prompt: currentPrompt, response: currentResponse}
+  const currentResponseObject = { prompt: currentPrompt, response: currentResponse }
   const renderedResponses = allResponses.map((responseObject) => {
     return <ResponseContainer key={responseObject.response.id} prompt={responseObject.prompt} response={responseObject.response.text} />
-  })  
-  
-  
+  })
+
+
   const clearResponses = () => {
     localStorage.removeItem('allResponses')
     setAllResponses([])
@@ -39,26 +58,38 @@ function App() {
   const getPrompt = (prompt) => {
     console.log(prompt, 'APP.JS LINE 14');
     setCurrentPrompt(prompt)
-    const configuration = new Configuration({
-      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-
-    openai.createCompletion("text-curie-001", {
-      prompt: `${prompt}`,
-      temperature: 0.7,
-      max_tokens: 100,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    })
-      .then((res) => {
-        console.log(res);
-        setCurrentResponse({
-          text: res.data.choices[0].text,
-          id: res.data.id
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `apikey/`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        const configuration = new Configuration({
+          apiKey: snapshot.val(),
+        });
+        const openai = new OpenAIApi(configuration);
+    
+        openai.createCompletion("text-curie-001", {
+          prompt: `${prompt}`,
+          temperature: 0.7,
+          max_tokens: 100,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
         })
-      })
+          .then((res) => {
+            console.log(res);
+            setCurrentResponse({
+              text: res.data.choices[0].text,
+              id: res.data.id
+            })
+          })
+    
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+
   }
 
   useEffect(() => {
